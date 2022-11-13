60f9f61a789d46a5558f968cedfb570ebe379e1d
Pushpak bhoite <bhoitepushpak6@gmail.com>
2022-11-13 17:15:12 +0530

    2nd Commit : Font end Step 1st Completed

--- crudproject/enroll/views.py
+++ crudproject/enroll/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from enroll.forms import StudentRegistration
 # Create your views here.
+
+def add_show(request):
+    if request.method == "POST":
+        fm = StudentRegistration(request.post)
+    else:
+        fm = StudentRegistration()
+    
+    return render(request , 'enroll/addandshow.html',{'form':fm})
+
+    
+
