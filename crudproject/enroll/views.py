a93bc25f2d7249ca28853b4e5068b39e9b5a96a5
Pushpak bhoite <bhoitepushpak6@gmail.com>
2022-11-14 00:18:14 +0530

    3rd Commit : Front page completed

--- crudproject/enroll/views.py
+++ crudproject/enroll/views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponseRedirect 
 from enroll.forms import StudentRegistration
+from enroll.models import User
 # Create your views here.
 
+# This Function will Add new Item and Show All Items
 def add_show(request):
     if request.method == "POST":
-        fm = StudentRegistration(request.post)
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm , email=em , password=pw )
+            reg.save()
+            fm = StudentRegistration()#this sentese shows blank data form
     else:
         fm = StudentRegistration()
-    
-    return render(request , 'enroll/addandshow.html',{'form':fm})
 
     
+    stud = User.objects.all()
+    return render(request , 'enroll/addandshow.html',{'form':fm ,'stu':stud})
+    
+    # This Function will Update
+def update_data(request , id ):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST ,instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST ,instance = pi)
 
+
+    return render(request , 'enroll/updatestudent.html' , {'id':id})
+
+
+
+    # This Function Will Delete 
+def delete_data(request , id ):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+    
